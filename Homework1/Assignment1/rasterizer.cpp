55b01b10685002c28b70b673c87f5e41da486786
huziyang.henry <huziyang.henry@foxmail.com>
2021-06-09 21:53:47 +0800

    添加README.md

--- Homework1/Assignment1/rasterizer.cpp
+++ Homework1/Assignment1/rasterizer.cpp
@@ -168,8 +168,8 @@ void rst::rasterizer::draw(rst::pos_buf_id pos_buffer, rst::ind_buf_id ind_buffe
         for (int i = 0; i < 3; ++i)
         {
             t.setVertex(i, v[i].head<3>());
-            t.setVertex(i, v[i].head<3>());
-            t.setVertex(i, v[i].head<3>());
+            // t.setVertex(i, v[i].head<3>());
+            // t.setVertex(i, v[i].head<3>());
         }
 
         t.setColor(0, 255.0, 0.0, 0.0);
