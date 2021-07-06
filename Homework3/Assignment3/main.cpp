5e4b0f7aac47d9c3d0d6c7174635900d7f8672b4
huziyang.henry <huziyang.henry@foxmail.com>
2021-07-06 20:58:27 +0800

    基础项的图片结果

--- Homework3/Assignment3/main.cpp
+++ Homework3/Assignment3/main.cpp
@@ -417,8 +417,8 @@ int main(int argc, const char **argv)
 
     rst::rasterizer r(700, 700);
 
-    // auto texture_path = "hmap.jpg";
-    auto texture_path = "spot_texture.png";
+    auto texture_path = "hmap.jpg";
+    // auto texture_path = "spot_texture.png";
     r.set_texture(Texture(obj_path + texture_path));
 
     std::function<Eigen::Vector3f(fragment_shader_payload)> active_shader = texture_fragment_shader;
